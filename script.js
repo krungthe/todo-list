d6ec2d6d64569629e21a18f42f7b0d6dfab496cc
krungthe <48580027+krungthe@users.noreply.github.com>
2019-04-22 18:39:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 var todoList = {
-todos: ['item 1', 'item 2', 'item 3'],
+todos: [],
 displayTodos: function(){
-  console.log('My Todos:', this.todos);
+  console.log('My Todos:');
+  for (var i=0; i < this.todos.length; i++) {
+    console.log(i+1, this.todos[i].todoText);
+  }
 },
-addTodo: function(todo) {
-  this.todos.push (todo);
+addTodo: function(todoText) {
+  this.todos.push ({
+    todoText: todoText,
+    completed: false
+  });
   this.displayTodos();
 },
-changeTodo: function(position,newValue) {
-  this.todos[position - 1] = newValue;
+changeTodo: function(position, todoText) {
+  this.todos[position - 1].todoText = todoText;
   this.displayTodos();
 },
 deleteTodo: function(position) {
