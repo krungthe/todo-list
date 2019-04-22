636cbd3cfd13441971c7022177278da60d7191dd
krungthe <48580027+krungthe@users.noreply.github.com>
2019-04-22 19:53:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
 var todoList = {
-todos: [],
-displayTodos: function(){
-  console.log('My Todos:');
-  for (var i=0; i < this.todos.length; i++) {
-    console.log(i+1, this.todos[i].todoText);
+  todos: [],
+  displayTodos: function() {
+   if (this.todos.length === 0) {
+     console.log("your todo list is empty!");
+   } else {
+      console.log('My Todos:');
+      for (var i=0; i < this.todos.length; i++) {
+       if (this.todos[i].completed === true) {
+        console.log("(x)" , i+1, this.todos[i].todoText);
+      } else {
+        console.log("( )" , i+1, this.todos[i].todoText);
+      }
+     }
+   }
+  },
+  addTodo: function(todoText) {
+    this.todos.push ({
+     todoText: todoText,
+      completed: false
+   });
+    this.displayTodos();
+  },
+  changeTodo: function(position, todoText) {
+   this.todos[position - 1].todoText = todoText;
+   this.displayTodos();
+  },
+  deleteTodo: function(position) {
+   this.todos.splice(position - 1,1);
+   this.displayTodos();
+  },
+  toggleCompleted: function(position) {
+    this.todos[position-1].completed = !this.todos[position-1].completed;
+    this.displayTodos();
   }
-},
-addTodo: function(todoText) {
-  this.todos.push ({
-    todoText: todoText,
-    completed: false
-  });
-  this.displayTodos();
-},
-changeTodo: function(position, todoText) {
-  this.todos[position - 1].todoText = todoText;
-  this.displayTodos();
-},
-deleteTodo: function(position) {
-  this.todos.splice(position - 1,1);
-  this.displayTodos();
-}
 };
